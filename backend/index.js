e298f88d8cf0dbedd7af71980c86d5005ba9b6a4
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-07-25 17:58:39 -0500

    add routes and model for Book

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,50 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import booksRoute from './routes/bookRoute.js';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? 1234;
+// Middleware for parsing request body
+app.use(express.json());
 
-app.listen(port, ()=>{
-    console.log(`listening on ; ${port}`);
-});
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome To MERN Stack Tutorial');
+  });
+  
+  app.use('/books', booksRoute);
+
+
+//Connection DATABASE
+export const connectDB = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`listening on ; ${port}`);
+    });
+
+    //mongoose.set('strictQuery', false);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Database connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectDB();
+
