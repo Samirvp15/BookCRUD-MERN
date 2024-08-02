d5a4d5c218a3679d96c4b877d0cfd2dc301c1cbe
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-08-02 10:21:58 -0500

    add axios and ReactRouters

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import {CreateBooks} from './pages/CreateBooks'
+import { ShowBooks } from './pages/ShowBooks'
+import { EditBooks } from './pages/EditBooks'
+import { DeleteBooks } from './pages/DeleteBooks'
+import { Home } from './pages/Home'
+
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>HOLAAA</div>
+    <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/books/create' element={<CreateBooks></CreateBooks>}></Route>
+       <Route path='/books/details/:id' element={<ShowBooks></ShowBooks>}></Route>
+       <Route path='/books/edit/:id' element={<EditBooks></EditBooks>}></Route>
+       <Route path='/books/delete/:id' element={<DeleteBooks></DeleteBooks>}></Route>
+    </Routes>
   )
 }
 
